6ba6d8331093908734d5233b8a529e7d002924c2
saudkhanbpk <saudkhanbpk@gmail.com>
2024-10-02 11:59:11 +0500

    added about page

--- src/app/contentiaio/about/page.tsx
+++ src/app/contentiaio/about/page.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import aboutImage from "../../../public/AboutImages/About .svg";
-import vectorImage from "../../../public/AboutImages/Vector.svg";
-import aboutcontact from "../../../public/AboutImages/aboutcontect.webp";
+import aboutImage from "../../../../public/AboutImages/About.svg";
+import vectorImage from "../../../../public/AboutImages/Vector.svg";
+import aboutcontact from "../../../../public/AboutImages/aboutcontact.jpg";
 import Image from "next/image";
-import Navbar from "@/components/navbar/Navbar";
 import { SiGooglemessages } from "react-icons/si";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { IoLocation } from "react-icons/io5";
 
 
 const About = () => {
- 
+
   return (
     <>
-      <Navbar />
-
       <div className="pt-28 px-4 lg:px-28">
         {/* Main Content Section */}
         <div className="flex flex-col lg:flex-row gap-10 justify-center items-center">
@@ -74,7 +71,7 @@ const About = () => {
             <h1 className="text-[#4D4EC9] font-bold text-2xl">İletişim</h1>
             <Image className="w-[140px]" src={vectorImage} alt="vectorImage" />
           </div>
-          
+
           <div className="flex flex-col justify-center mx-auto mt-6 mb-5">
             <div className="flex items-center gap-2">
               <SiGooglemessages size={30} />
@@ -94,26 +91,26 @@ const About = () => {
           </div>
 
           <div className="bg-gradient-to-r from-[#FFE2B2] to-[#F8A1B8] p-4 px-6 md:px-12 rounded-lg flex flex-col md:flex-row items-center gap-9 mx-auto w-full lg:w-[80vw] xl:w-[61vw] relative mb-6">
-  <div className="flex justify-center">
-    <Image className="w-[200px] h-auto" src={aboutcontact} alt="contectImage" />
-  </div>
-  <div className="w-full md:w-[700px]">
-    <h1 className="text-xl font-bold mb-5">İhtiyacınızı Belirleyelim</h1>
-    <div className="flex flex-col items-center">
-      <p>
-        Sorularınız ve ihtiyaçlarınız için yardıma mı ihtiyacınız var?
-        Hizmetlerimiz için bilgi almak adına temsilcimizle görüşme
-        gerçekleştirin!
-      </p>
-      <button className="bg-[#4D4EC9] text-white w-fit px-6 py-2 rounded-full font-bold mt-6">
-        İletişime Geç
-      </button>
-    </div>
-  </div>
-  <div className=" absolute bottom-3  left-0 w-full md:w-[280px] bg-purple-700 md:block hidden text-white p-6">
-    <h1 className="text-center">Will be provided later</h1>
-  </div>
-</div>
+            <div className="flex justify-center">
+              <Image className="w-[200px] h-auto" src={aboutcontact} alt="contectImage" />
+            </div>
+            <div className="w-full md:w-[700px]">
+              <h1 className="text-xl font-bold mb-5">İhtiyacınızı Belirleyelim</h1>
+              <div className="flex flex-col items-center">
+                <p>
+                  Sorularınız ve ihtiyaçlarınız için yardıma mı ihtiyacınız var?
+                  Hizmetlerimiz için bilgi almak adına temsilcimizle görüşme
+                  gerçekleştirin!
+                </p>
+                <button className="bg-[#4D4EC9] text-white w-fit px-6 py-2 rounded-full font-bold mt-6">
+                  İletişime Geç
+                </button>
+              </div>
+            </div>
+            <div className=" absolute bottom-3  left-0 w-full md:w-[280px] bg-purple-700 md:block hidden text-white p-6">
+              <h1 className="text-center">Will be provided later</h1>
+            </div>
+          </div>
 
         </div>
       </div>
