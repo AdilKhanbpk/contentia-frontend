a828a3e3142d9a207d5a678c4ca7411214831273
saudkhanbpk <saudkhanbpk@gmail.com>
2024-10-15 15:32:27 +0500

    payment endpoint created

--- src/components/orders/TabSecond.tsx
+++ src/components/orders/TabSecond.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from 'react';
-import Image from 'next/image';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export default function TabSecond() {
   const [orderDate, setOrderDate] = useState<Date>(new Date());
@@ -14,7 +15,7 @@ export default function TabSecond() {
   const deliveryEndDate = new Date(orderDate);
   deliveryEndDate.setDate(deliveryEndDate.getDate() + 35);
 
-  const formatDate = (date: Date) => {
+  const formatDate = (date: any) => {
     return date.toLocaleDateString('tr-TR', {
       day: 'numeric',
       month: 'long',
@@ -24,6 +25,27 @@ export default function TabSecond() {
   const handleCalendarClick = () => {
     setIsDatePickerOpen(!isDatePickerOpen);
   };
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data: any) => {
+    console.log(data); // Logs the form data
+
+    try {
+      const response = await axios.post('http://localhost:3001/api/v1/sipay/payment', data);
+      if (response.data.status === 'success') {
+        alert('Payment successful!');
+      } else {
+        alert('Payment failed: ' + response.data.errorMessage);
+      }
+    } catch (error) {
+      console.error('Payment error:', error);
+      alert('There was an issue with your payment.');
+    }
+  };
+
+
+
 
 
 
@@ -138,17 +160,20 @@ export default function TabSecond() {
 
         <div className='mt-2 sm:mt-2 md:mt-2 lg:mt-0 w-full lg:w-2/5'>
           <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="w-full  bg-white p-8 shadow-lg rounded-md">
+            <div className="w-full bg-white p-8 shadow-lg rounded-md">
               <h2 className="text-xl font-semibold mb-6">Ödeme</h2>
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+
                 {/* Card Number */}
                 <div>
                   <label className="block text-sm font-semibold mb-1">Kart Numarası</label>
                   <input
                     type="text"
                     placeholder="1111 2222 3333 4444"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("cardNumber", { required: true })}
                   />
+                  {errors.cardNumber && <span className="text-red-500">Kart numarası gerekli</span>}
                 </div>
 
                 {/* Expiry and CVV */}
@@ -158,16 +183,20 @@ export default function TabSecond() {
                     <input
                       type="text"
                       placeholder="AA/YY"
-                      className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                      {...register("expiryDate", { required: true })}
                     />
+                    {errors.expiryDate && <span className="text-red-500">Son kullanma tarihi gerekli</span>}
                   </div>
                   <div className="w-2/2 lg:w-1/2">
                     <label className="block text-sm font-semibold mb-1">CVV</label>
                     <input
                       type="text"
                       placeholder="***"
-                      className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                      {...register("cvv", { required: true })}
                     />
+                    {errors.cvv && <span className="text-red-500">CVV gerekli</span>}
                   </div>
                 </div>
 
@@ -177,27 +206,33 @@ export default function TabSecond() {
                   <input
                     type="text"
                     placeholder="Kart Üzerindeki İsim Soyisim"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("nameOnCard", { required: true })}
                   />
+                  {errors.nameOnCard && <span className="text-red-500">İsim soyisim gerekli</span>}
                 </div>
 
                 {/* Country and City */}
                 <div className="">
                   <label className="block text-sm font-semibold mb-1">Ülke</label>
-                  <select className="w-full px-3 py-2 border rounded-md focus:outline-none ">
-                    <option>Türkiye</option>
-                    {/* Add more countries as needed */}
+                  <select
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("country", { required: true })}
+                  >
+                    <option value="Türkiye">Türkiye</option>
                   </select>
+                  {errors.country && <span className="text-red-500">Ülke gerekli</span>}
                 </div>
+
                 <div className="">
                   <label className="block text-sm font-semibold mb-1">Contentia’yı Nereden Duydun</label>
                   <input
                     type="text"
                     placeholder="Opsiyonel"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("whereDidYouHear", { required: false })}
                   />
                 </div>
-
 
                 {/* Invoice Information */}
                 <h2 className="text-xl font-semibold mt-8 mb-4">Fatura Bilgileri</h2>
@@ -208,8 +243,10 @@ export default function TabSecond() {
                   <input
                     type="text"
                     placeholder="Ad Soyad / Şirket Unvanı"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("companyName", { required: true })}
                   />
+                  {errors.companyName && <span className="text-red-500">Ad Soyad / Şirket Ünvanı gerekli</span>}
                 </div>
 
                 {/* Tax/VAT ID */}
@@ -218,8 +255,10 @@ export default function TabSecond() {
                   <input
                     type="text"
                     placeholder="TCKN / VKN"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("taxId", { required: true })}
                   />
+                  {errors.taxId && <span className="text-red-500">TCKN / VKN gerekli</span>}
                 </div>
 
                 {/* Tax Office */}
@@ -228,7 +267,8 @@ export default function TabSecond() {
                   <input
                     type="text"
                     placeholder="Opsiyonel"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("taxOffice", { required: false })}
                   />
                 </div>
 
@@ -238,8 +278,10 @@ export default function TabSecond() {
                   <input
                     type="text"
                     placeholder="Açık Adres"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("address", { required: true })}
                   />
+                  {errors.address && <span className="text-red-500">Adres gerekli</span>}
                 </div>
 
                 {/* Email */}
@@ -248,8 +290,10 @@ export default function TabSecond() {
                   <input
                     type="email"
                     placeholder="E-Posta"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("email", { required: true })}
                   />
+                  {errors.email && <span className="text-red-500">E-posta gerekli</span>}
                 </div>
 
                 {/* Phone Number */}
@@ -257,47 +301,46 @@ export default function TabSecond() {
                   <label className="block text-sm font-semibold mb-1">Telefon Numarası</label>
                   <input
                     type="text"
-                    placeholder=""
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none "
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none"
+                    {...register("phoneNumber", { required: true })}
                   />
+                  {errors.phoneNumber && <span className="text-red-500">Telefon numarası gerekli</span>}
                 </div>
 
                 {/* Save Card Checkbox */}
                 <div className="flex items-center">
-                  <input type="checkbox" className="h-3 w-3  rounded border-gray-300 " />
+                  <input
+                    type="checkbox"
+                    className="h-3 w-3 rounded border-gray-300"
+                    {...register("saveCard")}
+                  />
                   <label className="ml-2 text-sm font-semibold">Kartımı Kaydet</label>
                 </div>
 
                 {/* Submit Button */}
-
-
                 <button type="submit" className="w-full ButtonBlue text-white px-4 py-2 rounded-md font-semibold">
                   <div className='flex flex-row space-x-8'>
                     <div className='w-1/4 flex justify-end items-center'>
-                      <Image
-                        src="/lockIcon.png" // Placeholder for the logo next to "Brand Name"
-                        alt="brand logo"
-                        height={20}
-                        width={20}
-                        className=""
-                      />
+                      <img src="/lockIcon.png" alt="brand logo" height={20} width={20} className="" />
                     </div>
                     <div className='w-3/4 flex justify-start items-center'>
                       Güvenli Ödeme
-
                     </div>
                   </div>
                 </button>
 
                 {/* Agreement Checkbox */}
                 <div className="flex items-center">
-                  <input type="checkbox" className="h-4 w-4 rounded border-gray-300 " />
+                  <input
+                    type="checkbox"
+                    className="h-4 w-4 rounded border-gray-300"
+                    {...register("agreement", { required: false })}
+                  />
                   <label className="ml-2 text-sm font-semibold">
                     Mesafeli Satış Sözleşmesini ve Ön Bilgilendirme Formunu Onaylıyorum
                   </label>
+                  {errors.agreement && <span className="text-red-500">Bu alan zorunludur</span>}
                 </div>
-
-
               </form>
             </div>
           </div>
