abd017d65693683b91ac296daa43c03beaa385a5
Saud Khan <99093073+saudkhanbpk@users.noreply.github.com>
2025-01-14 11:23:52 +0500

    Update axiosInstance.ts

--- src/store/axiosInstance.ts
+++ src/store/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "https://contentia-backend-s4pw.onrender.com/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
