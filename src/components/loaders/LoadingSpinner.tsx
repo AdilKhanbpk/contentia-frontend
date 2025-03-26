29106951c96fe283a12f03d3e39f6d901e3580c8
saudkhanbpk <saudkhanbpk@gmail.com>
2025-03-26 10:49:37 +0500

    Increase size of loading spinner for better visibility

--- src/components/loaders/LoadingSpinner.tsx
+++ src/components/loaders/LoadingSpinner.tsx
@@ -1,8 +1,8 @@
 // src/components/LoadingSpinner.tsx
 export default function LoadingSpinner() {
     return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-      </div>
+        <div className='flex items-center justify-center min-h-screen'>
+            <div className='animate-spin rounded-full h-16 w-16 border-y-4 border-gray-900'></div>
+        </div>
     );
-  }
+}
